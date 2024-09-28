fc82a076bd2a647dccf41db9a1c5aa9f952e753b
Justin Galimpin <jgalimpi@students.cs.ubc.ca>
2024-09-28 16:40:54 -0700

    Added specifications for Shop class

--- src/main/model/Shop.java
+++ src/main/model/Shop.java
@@ -4,29 +4,43 @@ import java.util.ArrayList;
 
 // Represents a shop having a name, inventory, and total money earned
 public class Shop {
-    private String name;
+    private String shopName;
     private ArrayList<Item> inventory;
-    private int totalMoneyEarned;
+    private int income;
 
-    public Shop() {
+    // REQUIRES: shopName is not an empty string
+    // EFFECTS: constructs a shop with given title, 0 income, and an 
+    //          empty inventory
+    public Shop(String shopName) {
         //stub
     }
 
-    public void addItem(){
+    // REQUIRES: 
+    // MODIFIES: this
+    // EFFECTS: adds item to inventory
+    public void addItem(Item item) {
         //stub
     }
 
-    public void sellItem(){
+    // REQUIRES: inventory contains the given item
+    // MODIFIES: this
+    // EFFECTS: removes item from inventory,
+    //          increases shop's income by item's cost
+    public void sellItem(Item item) {
         //stub
     }
 
-    public void modifyItem(){
+    // REQUIRES: inventory contains the given item
+    // MODIFIES: this, Item
+    // EFFECTS: changes the current value of a chosen quality of an item and
+    //          replaces it with a new value
+    public void modifyItem(Item item) {
         //stub
     }
 
-    public void viewInventory(){
+    // EFFECTS: returns a printed list of all item names in inventory
+    public void viewInventory() {
         //stub       
     }
     
-
 }
