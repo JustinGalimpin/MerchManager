0c70b8573ecc8bd5cd696bbe056eaddbb3332c26
Justin Galimpin <jgalimpi@students.cs.ubc.ca>
2024-09-28 18:36:44 -0700

    Added tests for Item class

--- src/main/model/Item.java
+++ src/main/model/Item.java
@@ -9,8 +9,7 @@ public class Item {
     private String description;
 
     // REQUIRES: itemName is not an empty string, cost > 0
-    // EFFECTS: constructs a shop with given title, 0 income, and an 
-    //          empty inventory    
+    // EFFECTS: constructs an with a given, type, rarity, cost, and description
     public Item(String itemName, String type, String rarity, int cost, String description) {
         this.itemName = itemName;
         this.type = type;
--- src/test/model/TestItem.java
+++ src/test/model/TestItem.java
@@ -1,5 +1,28 @@
 package model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class TestItem {
+
+    private Item sword;
+
+    @BeforeEach
+    void runBefore() {
+        sword = new Item("Sword", "Weapon", "Common", 10, "A basic sword.");
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("Sword", sword.getItemName());
+        assertEquals("Weapon", sword.getType());
+        assertEquals("Common", sword.getRarity());
+        assertEquals(10, sword.getCost());
+        assertEquals("A basic sword.", sword.getDescription());
+    }
+
+    
 
 }
