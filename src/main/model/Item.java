d72ac1a02e3fbd96ca48d5aed27e14b9fb3bda72
Justin Galimpin <jgalimpi@students.cs.ubc.ca>
2024-09-28 17:02:59 -0700

    Added parameters to Item constructor

--- src/main/model/Item.java
+++ src/main/model/Item.java
@@ -2,13 +2,16 @@ package model;
 
 // Represents an item having a name, type, rarity, cost, and description
 public class Item {
-    private String name;
+    private String itemName;
     private String type;
     private String rarity;
     private int cost;
     private String description;
 
-    public Item(){
+    // REQUIRES: itemName is not an empty string, cost > 0
+    // EFFECTS: constructs a shop with given title, 0 income, and an 
+    //          empty inventory    
+    public Item(String itemName, String type, String rarity, int cost, String description) {
         //stub
     }
 
